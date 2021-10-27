79ad161b9052b72e3108a151554ef6b3aa82666b
Afrin123456 <93269107+Afrin123456@users.noreply.github.com>
2021-10-27 22:33:56 +0600

    Add files via upload

---
+++ lab task 4/Model/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Public Home</title>
+    <style>
+    .ind h1 {
+
+        background-color: aquamarine;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans';
+        font-size: 50px;
+        margin-top: 10rem;
+        text-align: center;
+
+    }
+
+    .ind {
+        color: black;
+    }
+    </style>
+
+</head>
+
+<body>
+    <?php include './common/header.php';
+    include './common/footer.php';?>
+
+    <div class="ind">
+        <h1>Welcome to ABC company</h1>
+    </div>
+
+</body>
+
+</html>
